40875373b9d10bd740273ef9f5bbadeb0d13bee0
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2026-03-17 02:07:39 +0700

    add ui pipeline from monorepo

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+interface ButtonParams {
+  children: React.ReactNode;
+}
+
 export const Button = ({ children }: { children: React.ReactNode }) => {
   return <button>{children}</button>;
 };
